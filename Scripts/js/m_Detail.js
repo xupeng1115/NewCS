c6c444e1d723f36c60f2d220832b95df3529a2bb
xupeng1115 <15136723391@sina.cn>
2018-03-01 01:32:30 +0800

    添加高德地图

--- Scripts/js/m_Detail.js
+++ Scripts/js/m_Detail.js
@@ -18,28 +18,14 @@ var app=new Vue({
                 $("#address-container").css("height",parseInt(($(window).width()-30)/3.45)+"px");
             }
         });  
-        //绘制地图
+        //绘制百度地图
         (function(){
-            //通过地名获取经纬度
-            function searchByStationName() {
-                var point={};
-            　　localSearch.setSearchCompleteCallback(function (searchResult) {
-            　　　　 var poi = searchResult.getPoi(0);
-                    point=poi.point;
-            　　});
-            　　return point;
-            }
             //自定义地图标注图标
             function addMarker(point, index){  // 创建图标对象   
-                var myIcon = new BMap.Icon("../../Content/img/m_positioning2.png", new BMap.Size(15, 20), {    
-                    // 指定定位位置。   
-                    // 当标注显示在地图上时，其所指向的地理位置距离图标左上    
-                    // 角各偏移10像素和25像素。您可以看到在本例中该位置即是   
-                    // 图标中央下端的尖角位置。    
-                    anchor: new BMap.Size(10, 25),    
-                    // 设置图片偏移。   
-                    // 当您需要从一幅较大的图片中截取某部分作为标注图标时，您   
-                    // 需要指定大图的偏移位置，此做法与css sprites技术类似。    
+                var myIcon = new BMap.Icon("../../Content/img/m_positioning2.png", new BMap.Size(15, 20), { 
+                    //图片相对于地图中心的定位位置       
+                    anchor: new BMap.Size(10, 15),    
+                    // 设置图片偏移量   
                     imageOffset: new BMap.Size(0, 0 - index * 25)   // 设置图片偏移    
                 });      
                 // 创建标注对象并添加到地图   
@@ -47,19 +33,69 @@ var app=new Vue({
                 map.addOverlay(marker);    
             }
             
-            // 创建地图实例  BMap.point(经度，纬度)；注：使用其他坐标时要先转换为百度坐标
+            // 创建地图实例
             var map = new BMap.Map("address-container");
-            // 初始化地图，设置中心点坐标和地图级别
-            point = new BMap.Point(116.404, 39.915);
-            map.centerAndZoom(point, 11);
-
-            map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
-            map.enableContinuousZoom(true);    //启用地图惯性拖拽，默认禁用
-            //添加自定义标记
-            addMarker(point,0)
-            $(".BMap_Marker").css("z-index","10000");
+            //map.centerAndZoom(new BMap.Point(121.48, 31.22), 11);    //默认上海市经纬度
+            // 创建地址解析器实例     
+            var myGeo = new BMap.Geocoder();      
+            // 将地址解析结果显示在地图上，并调整地图视野    
+            myGeo.getPoint(app.positionDetail.Address, function(point){      
+                if (point) {   
+                    map.clearOverlays();      
+                    map.centerAndZoom(point, 11);       // 初始化地图，设置中心点坐标和地图级别
+                    addMarker(point,0)          //添加自定义标注图标  
+                }      
+            }, 
+            "上海市");
+            //监听地图加载完毕事件
+            map.addEventListener("tilesloaded",function(){$(".anchorBL").hide()});
         }());
 
+        //绘制高德地图
+        // (function(){
+        //     AMap.service('AMap.Geocoder',function(){//回调函数
+        //         //实例化Geocoder
+        //         geocoder = new AMap.Geocoder({
+        //             city: "010"//城市，默认：“全国”
+        //         });
+        //         var address=JSON.parse(resumeDetail).Address;
+        //         //TODO: 使用geocoder 对象完成相关功能
+        //         geocoder.getLocation(address, function(status, result) {
+        //             if (status === 'complete' && result.info === 'OK') {
+        //                 //TODO:获得了有效经纬度，可以做一些展示工作
+        //                 //比如在获得的经纬度上打上一个Marker
+        //                 var map = new AMap.Map('address-container',{
+        //                     resizeEnable: true,
+        //                     zoom: 10,
+        //                     center: [result.geocodes[0].location.lng,result.geocodes[0].location.lat]
+        //                 });
+        //                 var icon = new AMap.Icon({
+        //                     image : '../../Content/img/m_positioning2.png',//24px*24px
+        //                     //icon可缺省，缺省时为默认的蓝色水滴图标，
+        //                     size : new AMap.Size(15,20)
+        //                 });
+        //                 var marker = new AMap.Marker({
+        //                     icon : icon,//24px*24px
+        //                     position : [result.geocodes[0].location.lng,result.geocodes[0].location.lat],
+        //                     offset : new AMap.Pixel(-10,-30),
+        //                     map : map
+        //                 });
+        //                 map.on("complete",function(){
+        //                     $(".amap-logo").remove();
+        //                     $(".amap-copyright").remove();
+                            
+        //                 })
+        //             }else{
+        //                 var map = new AMap.Map('address-container',{
+        //                     resizeEnable: true,
+        //                     zoom: 10,
+        //                     center: [121.48,31.22]
+        //                 });
+        //                 alert("获取工作地址信息失败了！");
+        //             }
+        //         });
+        //     });
+        // }());   
     });
 }());
 
@@ -69,14 +105,3 @@ function noFindLogo() {
     oImg.src = "../../Content/img/position_head.png";
     oImg.onerror = null;
 }
-
-//通过城市名获取经纬度
-function theLocation(){
-    var city = document.getElementById("cityName").value;
-    if(city != ""){
-        map.centerAndZoom(city,11);      // 用城市名设置地图中心点
-    }
-}
-
-
-
