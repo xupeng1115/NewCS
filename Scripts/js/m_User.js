f71866ae0389086e4727ad29b7a2dddb2096de58
markxu <15136723391@sina.cn>
2018-05-31 18:51:21 +0800

    添加登陆页

---
+++ Scripts/js/m_User.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        lognFocus:false,
+    },
+    methods: {
+        
+    }
+})
+
+$(function () {
+
+    // $("body").on("touchstart", ".flo-container", function (event) {
+
+    // });
+
+});
+
+
