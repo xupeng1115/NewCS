bc815d7cc0a4c04503ab6cd79865e7265ea25f0e
markxu <15136723391@sina.cn>
2018-05-31 13:27:52 +0800

    优化首页

--- Scripts/js/m_HomeIndex.js
+++ Scripts/js/m_HomeIndex.js
@@ -1,51 +1,63 @@
-var app=new Vue({
-    el:"#app",
-    data:{
-        menuKey:false
+var app = new Vue({
+    el: "#app",
+    data: {
+        menuKey: false
     },
-    methods:{
-        menuHandle:function(event){
-            if(app.menuKey){
-                app.menuKey=false;
-            }else{
-                app.menuKey=true;
+    methods: {
+        menuHandle: function (event) {
+            if (app.menuKey) {
+                app.menuKey = false;
+            } else {
+                app.menuKey = true;
             }
         }
     }
 })
 
-$(function(){
-
+$(function () {
     //“想从事的行业”
-    var mySwiper1 = new Swiper('.swiper-container1', {
-        autoplay:3000,
-        speed:700,
-        slidesPerView :"auto",
-        slidesPerGroup : 1,
-        freeMode : true,
-        freeModeMomentum : true,
-    });
+    var mySwiper1 = new Swiper('#swiper1', {
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: true,
+        },
+        speed: 500,
+        slidesPerView: "auto",
+        slidesPerGroup: 1,
+        freeMode: true,
+        freeModeMomentum: true,
+        });
 
     //“理想工作怎么找”
-    var mySwiper2 = new Swiper('.swiper-container2', {
-        autoplay:2000,
+    var mySwiper2 = new Swiper('#swiper2', {
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: true,
+        },
         speed:700,
-        slidesPerView :1,
-        slidesPerGroup : 1,
-        freeMode : true,
-        freeModeMomentum : true,
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        freeMode: true,
+        freeModeMomentum: true,
 
-        pagination : '.swiper-pagination2',
-        paginationHide :true,
+        pagination: {
+            el: '.swiper-pagination2',
+            bulletElement: 'li',
+            clickable: true,
+        }
+        
     });
 
     //“合作企业”
-    var mySwiper3 = new Swiper('.swiper-container3', {
+    var mySwiper3 = new Swiper('#swiper3', {
         loop:true,
-        autoplay:1,
+        autoplay: {
+            delay:0,
+            disableOnInteraction: true,
+        },
         speed:5000,
-        slidesPerView :"auto",
-        slidesPerGroup : 1,
+        slidesPerView: "auto",
+        slidesPerGroup: 1,
         loopedSlides :1,
         freeMode : true,
         freeModeMomentum : true,
@@ -57,7 +69,8 @@ $(function(){
         'menu': document.getElementById('menu'),
         'padding': 190,
         'tolerance': 70,
-        'side':"right"
+        'side': "right",
+        'touch': false
     });
 
     $("body").on("click",".top-menu",function(){
@@ -79,4 +92,12 @@ $(function(){
     slideout.on('open', function() {
         app.menuKey=true;
     });
-})
+    
+    $("body").on("touchstart", ".flo-container", function (event) {
+        event.preventDefault();
+        slideout.close();
+    });
+
+});
+
+
--- Scripts/js/m_Template.js
+++ Scripts/js/m_Template.js
@@ -1,3 +1,10 @@
+//引入fastclick.js事件快速响应处理
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function () {
+        FastClick.attach(document.body);
+    }, false);
+}
+
 //Ajax
 function myAjax(myType, myUrl, myParams, mySuccessFun, myErrorFun) {
     var params = {
@@ -32,4 +39,35 @@ function getAjax(myType, myUrl, myParams, mySuccessFun, myErrorFun) {
     } else {
         communication.post(params, successFun, errorFun);
     }
+}
+
+//替换回车和换行符
+function TransferString(content) {
+    var string = content;
+    try {
+        string = string.replace(/\r\n/g, "<br>")
+        string = string.replace(/\n/g, "<br>");
+    } catch (e) {
+        console.log(e.message);
+    }
+    return string;
+}
+
+//禁止用户缩放页面
+window.onload = function () {
+    document.addEventListener('touchstart', function (event) {
+        if (event.touches.length > 1) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+    })
+    var lastTouchEnd = 0;
+    document.addEventListener('touchend', function (event) {
+        var now = (new Date()).getTime();
+        if (now - lastTouchEnd <= 300) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        lastTouchEnd = now;
+    }, false)
 }
